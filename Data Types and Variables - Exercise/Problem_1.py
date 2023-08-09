6ff64d145a4b30dfb3281bb60860e199ff3144b8
Mihail <mihail.22@abv.bg>
2023-08-09 11:33:11 +0300

    01. Integer Operations

---
+++ Data Types and Variables - Exercise/Problem_1.py
@@ -0,0 +1,12 @@
+# 01. Integer Operations
+
+a = int(input())
+b = int(input())
+c = int(input())
+d = int(input())
+
+intermediate_sum = a + b
+division_result = intermediate_sum // c
+total_sum = division_result * d
+
+print(total_sum)
