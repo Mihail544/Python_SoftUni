a32abb28b8fdb002566c9f4b64699cd9eae7003b
Mihail <mihail.22@abv.bg>
2023-08-11 19:27:36 +0300

    03. Decrypting Messages

---
+++ Data Types and Variables - More Exercises/Problem_3.py
@@ -0,0 +1,24 @@
+# 3. Decrypting Messages
+
+key = int(input())
+n = int(input())
+
+list_of_crypt = []
+decrypted = []
+answer = []
+
+for _ in range(n):
+    crypt = input()
+    list_of_crypt.append(crypt)
+
+for crypted in list_of_crypt:
+    decrypted.append(ord(crypted)+key)
+
+new_list_of_crypt = decrypted
+
+for numbers in new_list_of_crypt:
+    answer.append(chr(numbers))
+
+
+End = "".join(answer)
+print(End)
