b7b79a0a066585e8c61a617e57fb44cee3c667bd
Mihail <mihail.22@abv.bg>
2023-08-14 20:45:45 +0300

    04. Balanced Brackets
    Fix , just a try to make it work

--- Data Types and Variables - More Exercises/Problem_4.py
+++ Data Types and Variables - More Exercises/Problem_4.py
@@ -1,21 +1,21 @@
 # 4. Balanced Brackets
 
-def is_balanced(expression):
-    stack = []
-    for char in expression:
-        if char == "(":
-            stack.append(char)
-        elif char == ")":
-            if not stack or stack[-1] != "(":
-                return False
-            stack.pop()
-    return not stack
-
 n = int(input())
 
+stack = []
+
 for _ in range(n):
-    line = input().strip()
-    if is_balanced(line):
-        print("BALANCED")
-    else:
-        print("UNBALANCED")
+    line = input()
+
+    if line == "(":
+        stack.append("(")
+    elif line == ")":
+        if len(stack) == 0 or stack[-1] != "(":
+            print("UNBALANCED")
+            break
+        stack.pop()
+
+if len(stack) == 0:
+    print("BALANCED")
+else:
+    print("UNBALANCED")
