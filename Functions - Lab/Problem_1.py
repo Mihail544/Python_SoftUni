b2f7a14ad3cc126293926bd49c4222b0d79f78a6
Mihail <mihail.22@abv.bg>
2024-04-19 19:16:15 +0300

    01. Absolute Values

---
+++ Functions - Lab/Problem_1.py
@@ -0,0 +1,6 @@
+# 1. Absolute Values
+
+numbers = input().split(" ")
+numbers = [float(number) for number in numbers]
+numbers = [abs(number) for number in numbers]
+print(numbers)
--- Lists Basics - More Exercises/Problem_6.py
+++ Lists Basics - More Exercises/Problem_6.py
@@ -3,6 +3,7 @@
 list_of_numbers = input().split(" ")
 list_of_numbers = [int(numbers) for numbers in list_of_numbers]
 
+
 while True:
     command = input().split(" ")
 
@@ -21,80 +22,68 @@ while True:
 
     elif command[0] == "max" or command[0] == "min":
         odd_or_even = command[1]
-
         even_list = [int(number) for number in list_of_numbers if number % 2 == 0]
         odd_list = [int(number) for number in list_of_numbers if number % 2 == 1]
 
         if command[0] == "max":
+            if odd_or_even == "even" and even_list:
 
-            if odd_or_even == "even":
-                try:
-                    max_index = even_list.index(max(even_list))
-                    print(max_index)
-                except ValueError:
-                    print("No matches")
+                max_even = max(even_list)
+                max_even_index = list_of_numbers.index(max_even)
+                print(max_even_index)
 
-            elif odd_or_even == "odd":
-                try:
-                    max_index = odd_list.index(max(odd_list))
-                    print(max_index)
-                except ValueError:
-                    print("No matches")
+            elif odd_or_even == "odd" and odd_list:
+                max_odd = max(odd_list)
+                max_odd_index = list_of_numbers.index(max_odd)
+                print(max_odd_index)
+
+            else:
+                print("No matches")
 
         elif command[0] == "min":
+            if odd_or_even == "even" and even_list:
 
-            if odd_or_even == "even":
-                try:
-                    min_index = even_list.index(min(even_list))
-                    print(min_index)
-                except ValueError:
-                    print("No matches")
+                min_even = min(even_list)
+                min_even_index = list_of_numbers.index(min_even)
+                print(min_even_index)
 
-            elif odd_or_even == "odd":
-                try:
-                    min_index = odd_list.index(min(odd_list))
-                    print(min_index)
-                except ValueError:
-                    print("No matches")
+            elif odd_or_even == "odd" and odd_list:
+                min_odd = min(odd_list)
+                min_odd_index = list_of_numbers.index(min_odd)
+                print(min_odd_index)
+
+            else:
+                print("No matches")
 
     elif command[0] == "first" or command[0] == "last":
-        count = int(command[1])
-        odd_or_even = command[2]
-
         even_list = [int(number) for number in list_of_numbers if number % 2 == 0]
         odd_list = [int(number) for number in list_of_numbers if number % 2 == 1]
 
-        if command[0] == "first":
+        count = int(command[1])
+        odd_or_even = command[2]
 
-            if odd_or_even == "even":
-                if count >= len(even_list):
+        if count > len(list_of_numbers) and count > 0:
+            print("Invalid count")
+        else:
+
+            if command[0] == "first":
+
+                if odd_or_even == "even":
                     first_even = even_list[:count]
                     print(first_even)
-                else:
-                    print("Invalid count")
 
-            elif odd_or_even == "odd":
-                if count >= len(odd_list):
+                elif odd_or_even == "odd":
                     first_odd = odd_list[:count]
                     print(first_odd)
-                else:
-                    print("Invalid count")
 
-        elif command[0] == "last":
+            elif command[0] == "last":
 
-            if odd_or_even == "even":
-                if count >= len(even_list):
+                if odd_or_even == "even":
                     last_even = even_list[-count:]
                     print(last_even)
-                else:
-                    print("Invalid count")
 
-            elif odd_or_even == "odd":
-                if count >= len(odd_list):
+                elif odd_or_even == "odd":
                     last_odd = odd_list[-count:]
                     print(last_odd)
-                else:
-                    print("Invalid count")
-
 
 print(list_of_numbers)
